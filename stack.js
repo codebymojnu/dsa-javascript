2aba65dd20ab4db53bac86b83c7270fa460a04ba
MD. MOJNU MIAH <94952925+jsmojnu@users.noreply.github.com>
2022-03-17 09:52:42 +0600

    stack

---
+++ stack.js
@@ -0,0 +1,41 @@
+// program to implement stack data structure
+
+class Stack{
+    constructor(){
+        this.items = [];
+    }
+
+    // add item to stack
+    add(element){
+        return this.items.push(element);
+    }
+
+    // remove item from the stack
+    remove(element){
+        if(this.items.length > 0){
+            return this.items.pop(element);
+        }
+    }
+
+    // stack size
+    size(){
+        return this.items.length;
+    }
+
+    // clear the stack
+    clear(){
+       this.items = [];
+    }
+}
+
+const stack = new Stack();
+
+stack.add(1);
+stack.add(2);
+stack.add(3);
+console.log(stack.items);
+stack.remove();
+console.log(stack.size());
+stack.clear();
+console.log(stack);
+
