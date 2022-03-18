c048298141dd7a7d2bba6e542fcd3de5cb0c09f6
MD. MOJNU MIAH <94952925+jsmojnu@users.noreply.github.com>
2022-03-18 17:21:31 +0600

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+const bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if(arr[j] > arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            }
+        }
+    }
+    return arr;
+}
+
+const result = bubbleSort([9, 1, 3, 7]);
+console.log(result);
